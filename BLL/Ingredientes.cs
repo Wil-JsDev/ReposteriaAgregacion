ba5ddca8529593e257e718e32f1562a675292aef
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-03-29 12:13:07 -0500

    Clases ingredientes y productos agregadas

---
+++ BLL/Ingredientes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Ingredientes
+    {
+        #region Propiedades
+        public string Nombre { get; set; }
+        public double Cantidad { get; set; }
+        public double Precio { get; set; }
+        #endregion  
+    }
+}
---
+++ BLL/Producto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Producto : Ingredientes
+    {
+        #region Propiedades
+        public string Nombre { get; set; }
+        public string Caracteristicas { get; set;}
+        public List<Ingredientes> ListaIngredientes = new List<Ingredientes>();
+        #endregion
+
+        #region Metodos 
+        public void AgregarIngredientes(Ingredientes ingredientes)
+        {
+            ListaIngredientes.Add(ingredientes);
+        }
+
+        public void CalcularCosto()
+        {
+            double costo = 0;
+            foreach (var ingredientes in ListaIngredientes)
+            {
+                costo += ingredientes.Precio;    
+            }
+            Precio = costo;
+        }
+
+        public bool QuitarIngredientes(Ingredientes ingredientes)
+        {
+           var contains = ListaIngredientes.Contains(ingredientes);
+
+            if (contains)
+            {
+                ListaIngredientes.Remove (ingredientes);
+                return true;
+            } 
+            
+            return false;
+        }
+
+
+        #endregion
+    }
+}
